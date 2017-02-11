d7d9231393e04fb43e1b11843d7eebd84acceef1
chungw51993 <chungw51993@gmail.com>
2017-02-10 19:31:40 -0600

    Stopping point - still need to add asteroid image to circle elements
    > need to add draggable player piece

--- client/watchout.js
+++ client/watchout.js
@@ -2,7 +2,7 @@
 var gameOptions = {
   height: 450,
   width: 700,
-  nEnemies: 30,
+  nEnemies: 10,
   padding: 20
 };
 
@@ -11,29 +11,31 @@ var gameScore = {
   hiscore: 0
 };
 
-// var axes = {
-//   x: d3.scale
-//        .linear()
-//        .domain([0, 100])
-//        .range([0, gameOptions.width]),
+var axes = {
+  x: d3.scale
+       .linear()
+       .domain([0, 100])
+       .range([0, gameOptions.width]),
 
-//   y: d3.scale
-//        .linear()
-//        .domain([0, 100])
-//        .range([0, gameOptions.height])
-// };
+  y: d3.scale
+       .linear()
+       .domain([0, 100])
+       .range([0, gameOptions.height])
+};
 
 var gameBoard = d3.select('.board').append('svg:svg')
                   .attr('width', gameOptions.width)
                   .attr('height', gameOptions.height);
 
+//gameBoard.append('pattern:pattern').attr('id', 'image').append('image:image').attr('xlink:href', 'asteroid.png');
 
 var makeEnemies = function (nEnemies) {
   var enemyArray = [];
   for (var i = 0; i < nEnemies; i++) {
-    var enemies = gameBoard.append('svg:circle')
+    var enemies = gameBoard.append('circle:circle')
                     .attr('id', 'enemy' + i)
                     .attr('class', 'enemy')
+                    .attr('xlink:href', 'asteroid.png')
                     .attr('cx', Math.random() * gameOptions.width)
                     .attr('cy', Math.random() * gameOptions.height)
                     .attr('r', 10);
@@ -46,11 +48,18 @@ var moveEnemies = function (enemyData) {
   for (var i = 0; i < enemyData.length; i++) {
     gameBoard.select('#enemy' + i)
       .transition()
-      .duration(1000)
+      .duration(1500)
       .attr('cx', Math.random() * gameOptions.width)
       .attr('cy', Math.random() * gameOptions.height);
     gameScore.score++;
   }
 };
 var allEnemies = makeEnemies(gameOptions.nEnemies);
-setInterval(function () {moveEnemies(allEnemies)}, 1500);
+setInterval(function () { moveEnemies(allEnemies); }, 2000);
+
+var collision = function(allEnemyPositions, mousePosition) {
+  // if allEnemyPositions x & y intersect with mousePosition x & y
+    // reset current score to 0
+  // if current score > high score
+    // high score = current score
+};
