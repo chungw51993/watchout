139a63937276a5005307e115b1236f1f30187c65
chungw51993 <chungw51993@gmail.com>
2017-02-11 11:23:20 -0600

    Cartman is working - kinda

--- client/watchout.js
+++ client/watchout.js
@@ -2,7 +2,7 @@
 var gameOptions = {
   height: 450,
   width: 700,
-  nEnemies: 10,
+  nEnemies: 30,
   padding: 20
 };
 
@@ -32,11 +32,11 @@ var makeEnemies = function (nEnemies) {
   var enemyArray = [];
   for (var i = 0; i < nEnemies; i++) {
     var enemies = gameBoard.append('image')
-                    .attr('id', 'enemy' + i)
-                    .attr('class', 'enemy')
-                    .attr('x', Math.random() * gameOptions.width)
-                    .attr('y', Math.random() * gameOptions.height)
-                    .attr('xlink:href', 'asteroid.png');
+                          .attr('id', 'enemy' + i)
+                          .attr('class', 'enemy')
+                          .attr('x', Math.random() * gameOptions.width)
+                          .attr('y', Math.random() * gameOptions.height)
+                          .attr('xlink:href', 'asteroid.png');
     enemyArray.push(enemies);
   }
   return enemyArray;
@@ -48,12 +48,38 @@ var moveEnemies = function (enemyData) {
       .transition()
       .duration(1500)
       .attr('x', Math.random() * gameOptions.width)
-      .attr('y', Math.random() * gameOptions.height);
+      .attr('y', Math.random() * gameOptions.height)
     gameScore.score++;
   }
 };
 var allEnemies = makeEnemies(gameOptions.nEnemies);
 setInterval(function () { moveEnemies(allEnemies); }, 2000);
+
+
+var makePlayer = function () {
+
+
+  var player = gameBoard.append('image')
+                        .attr('class', 'player')
+                        .attr('x', gameOptions.width / 2)
+                        .attr('y', gameOptions.height / 2)
+                        .attr('xlink:href', 'cartman.png');
+};
+
+var movePlayer = function() {
+  var mouseXY = d3.select('.board').on('mouseover', function () {
+    return d3.mouse(this);
+    console.log(mouseXY);
+  });
+  console.log(mouseXY);
+  gameBoard.select('.player')
+           .transition()
+           .attr('x', mouseXY[0])
+           .attr('y', mouseXY[1])
+}
+
+makePlayer();
+setInterval(function() { movePlayer(); }, 500);
 
 var collision = function(allEnemyPositions, mousePosition) {
   // if allEnemyPositions x & y intersect with mousePosition x & y
